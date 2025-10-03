f7201ffb5e63268950ee2aa2ab5b1daf9b5c3754
DanteAnthony <dg.anthony0604@gmail.com>
2025-10-03 14:18:53 -0700

    v1.1

--- astroid.py
+++ astroid.py
@@ -1,12 +1,13 @@
 import pygame
 from pygame.locals import *
+import random
 
 pygame.init()
 
 clock = pygame.time.Clock()
 fps = 60
 
-# screen size
+# screen set up
 screen_width = 936
 screen_height = 936
 
@@ -15,6 +16,21 @@ pygame.display.set_caption('Astroid')
 
 bg = pygame.image.load("astroid/img/spacebg.png")
 
+# Player class
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("astroid/img/player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        pass
+
+player_group = pygame.sprite.Group()
+player = Player(int(screen_width / 2), int(screen_height / 2))
+player_group.add(player)
+
 # game run
 run = True
 while run:
@@ -22,6 +38,8 @@ while run:
     clock.tick(fps)
 
     screen.blit(bg, (0, 0))
+
+    player_group.draw(screen)
 
     # stop / start game
     for event in pygame.event.get():
